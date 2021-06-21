ad9b564f2896912a1b32251c3185295ad44b4af6
Edvin Maid <edvvin@mail.com>
2021-06-22 01:02:40 +0200

    Frontend work 2

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -8,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
+  
+  username : string;
+  password: string;
+  userType: boolean;
 
   ngOnInit(): void {
+    this.username = "user";
+    this.password = "user";
+    this.userType = true;
   }
 
 }
